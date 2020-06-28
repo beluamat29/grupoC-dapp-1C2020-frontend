93b8e327136f2ce181fc5b01d5c3ee32cba4bc09
belu <belen.amat29@gmail.com>
2020-06-27 23:14:30 -0300

    marcando datos en los campos

--- src/pages/user-profile/StoreAdminData.js
+++ src/pages/user-profile/StoreAdminData.js
@@ -2,31 +2,51 @@ import * as React from "react";
 import {LanguageContext} from "../../constants/LanguageMaps";
 import "./user-profile.scss"
 import {openingDays, paymentMethods, storeCategories} from "../../constants/Constants";
-import {categories} from "../../constants";
 
 class StoreAdminData extends React.Component {
-    generateEntityCheckbox = (entity, onSelectEntity) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: this.props.user.username,
+            password: this.props.user.password,
+            store: this.props.user.store
+        }
+    }
+
+    componentDidMount() {
+        this.setState({store: this.props.user.store})
+    }
+
+    generateEntityCheckbox = (entity, isSelectedCheckbox = false) => {
         return (
             <div className="rubro-checkbox">
-                <input type="checkbox" value={entity.value} onClick={(event) => onSelectEntity(event.target.value)}/>
+                <input type="checkbox"
+                       value={entity.value}
+                       checked={isSelectedCheckbox}
+                />
                 <label className="checkbox">
                     {entity.label}
                 </label>
             </div>
         )
     }
+
+    isSelectedDay = (day) => this.state.store.storeSchedule.openingDays.includes(day.value);
+    isSelectedCategory = (category) => this.state.store.storeCategories.includes(category.value);
+    isSelectedPaymentMethod = (method) => this.state.store.availablePaymentMethods.includes(method.value);
+
     render() {
         return(
             <div className="user-profile-data-panel">
                 <div className="user-profile-data-container store-admin">
                     <div className="user-profile-data-input">
                         <label>{this.context.userProfileUsername}</label>
-                        <span>{this.props.user.username}</span>
+                        <span>{this.state.username}</span>
                     </div>
                     <div className="user-profile-data-input">
                         <label>{this.context.userProfilePassword}</label>
                         <input type="password" id="user-password" name="user-password"
-                               value={this.props.user.password}
+                               value={this.state.password}
                                onChange={(event) => this.props.updateUserField('password', event.target.value)}/>
                     </div>
                     <div className="user-profile-data-save-button">
@@ -40,30 +60,30 @@ class StoreAdminData extends React.Component {
                         <div className="store-data-text-input">
                             <label>{this.context.storeName}</label>
                             <input type="text" id="user-password" name="user-password"
-                                   value={this.props.user.store.storeName}
+                                   value={this.state.store.storeName}
                                    onChange={(event) => this.props.updateUserField('password', event.target.value)}/>
                         </div>
 
                         <div className="store-data-text-input">
                             <label>{this.context.storeAddress}</label>
                             <input type="text" id="store-address" name="store-address"
-                                   value={this.props.user.store.storeAddress}
+                                   value={this.state.store.storeAddress}
                                    onChange={(event) => this.props.updateUserField('password', event.target.value)}/>
                         </div>
                         <div className="store-data-time-input">
                             <label>{this.context.storeSchedule}</label>
                             <div className="times">
-                                <input type="time" name="start-time" value={this.props.user.store.storeSchedule.openingTime}
+                                <input type="time" name="start-time" value={this.state.store.storeSchedule.openingTime}
                                        onChange={(event) => this.props.onUpdate('openingTime', event.target.value)}/>
                                 <span>{this.context.toTime}</span>
-                                <input type="time" name="end-time" value={this.props.user.store.storeSchedule.closingTime}
+                                <input type="time" name="end-time" value={this.state.store.storeSchedule.closingTime}
                                        onChange={(event) => this.props.onUpdate('closingTime', event.target.value)}/>
                             </div>
                         </div>
                         <div className="store-data-days">
                             <label>{this.context.storeDataOpeningDays}</label>
                             <div className="categories-grid">
-                                {openingDays.map(day => this.generateEntityCheckbox(day))}
+                                {openingDays.map(day => this.generateEntityCheckbox(day, this.isSelectedDay(day)))}
                             </div>
                         </div>
                     </div>
@@ -72,13 +92,13 @@ class StoreAdminData extends React.Component {
                         <div className="store-data-days">
                             <label>{this.context.storeDataCategories}</label>
                             <div className="categories-grid">
-                                {storeCategories.map(day => this.generateEntityCheckbox(day))}
+                                {storeCategories.map(category => this.generateEntityCheckbox(category, this.isSelectedCategory(category)))}
                             </div>
                         </div>
                         <div className="store-data-days">
                             <label>{this.context.storeDataPaymentMethods}</label>
                             <div className="categories-grid">
-                                {paymentMethods.map(day => this.generateEntityCheckbox(day))}
+                                {paymentMethods.map(method => this.generateEntityCheckbox(method, this.isSelectedPaymentMethod(method)))}
                             </div>
                         </div>
                         <div className="store-data-text-input">
